b0edf3ee372e37639a747ad1fef9714f27068824
codekot <boobookish@gmail.com>
2021-07-09 15:04:05 +0400

    Add margin

--- main.py
+++ main.py
@@ -1,44 +1,58 @@
 from random import randint
 
 from kivy.app import App
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ListProperty
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.vector import Vector
 
 
 class Cell(Widget):
-    def __init__(self, x, y, size):
+    graphical_size = ListProperty([1,1])
+    graphical_pos = ListProperty([1,1])
+
+    def __init__(self, x, y, size, margin=4):
         super().__init__()
-        self.size = (size, size)
-        self.pos = (x, y)
+        self.actual_size = (size, size)
+        self.graphical_size = (size - margin, size - margin)
+        self.margin = margin
+        self.actual_pos = (x, y)
+        self.graphical_pos_attach()
         velocity_x = NumericProperty(0)
         velocity_y = NumericProperty(0)
         velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    def graphical_pos_attach(self):
+        self.graphical_pos = (self.actual_pos[0] - self.graphical_size[0] / 2,
+                              self.actual_pos[1] - self.graphical_size[1] / 2)
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
 class Form(Widget):
     def __init__(self):
         super().__init__()
-        self.cells = []
+        self.cell1 = Cell(100,100,30)
+        self.cell2 = Cell(130,100,30)
+        self.add_widget(self.cell1)
+        self.add_widget(self.cell2)
+        # self.cells = []
 
     def start(self):
         Clock.schedule_interval(self.update, 0.01)
 
     def update(self, _):
-        for cell in self.cells:
-            # cell.pos = (cell.pos[0] + 2, cell.pos[1] + 3)
-            if not hasattr(cell, "velocity"):
-                cell.velocity = Vector(3,0).rotate(randint(0,360))
-            cell.move()
+        # for cell in self.cells:
+        #     # cell.pos = (cell.pos[0] + 2, cell.pos[1] + 3)
+        #     if not hasattr(cell, "velocity"):
+        #         cell.velocity = Vector(3,0).rotate(randint(0,360))
+        #     cell.move()
+        pass
 
 
-    def on_touch_down(self, touch):
-        cell = Cell(touch.x, touch.y, 30)
-        self.add_widget(cell)
-        self.cells.append(cell)
+    # def on_touch_down(self, touch):
+    #     cell = Cell(touch.x, touch.y, 30)
+    #     self.add_widget(cell)
+    #     self.cells.append(cell)
 
 class WormApp(App):
     def build(self):
